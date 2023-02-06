41941215d297483abc1457a22e239081407ad0a5
matiev-ruslan <matiev.ruslan@gmail.com>
2023-02-06 21:00:20 +0400

    Add alert

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
+
 
 
 def go_to_login_page(browser):
@@ -14,8 +16,11 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
